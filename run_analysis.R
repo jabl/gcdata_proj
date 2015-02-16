7058fa781a5299111f466c368e28cc403ae12202
Janne Blomqvist <blomqvist.janne@gmail.com>
2015-02-17 00:45:45 +0200

    Begin creating tidy dataset

--- run_analysis.R
+++ run_analysis.R
@@ -6,4 +6,30 @@ getUCIHARData <- function() {
                       destfile=fname, method="curl")
         unzip(fname)
     }
+    # Return root directory of uncompressed data
+    "UCI HAR Dataset"
 }
+
+# Run everything
+runAll <- function() {
+  udir <- getUCIHARData()
+
+  # Create a factor with the activity labels
+  al <- read.table(file.path(udir, "activity_labels.txt"),
+                   colClasses=c("integer", "character"))
+  act_lab <- factor(al$V1, al$V2)
+
+  # Get the feature labels
+  feat_lab <- read.table(file.path(udir, "features.txt"),
+                         colClasses=c("NULL", "character"))$V2
+
+  xtest <- read.table(file.path(udir, "test", "X_test.txt"),
+                      colClasses="numeric", comment.char="",
+                      col.names=feat_lab)
+  ytest <- read.table(file.path(udir, "test", "y_test.txt"),
+                      colClasses="factor")
+  stest <- read.table(file.path(udir, "test", "subject_test.txt"),
+                      colClasses="factor")
+}
+
+runAll()
