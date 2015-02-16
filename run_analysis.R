526f0d8b8080cc82fa2bde7d44475bd8312f8a38
Janne Blomqvist <janne.blomqvist@aalto.fi>
2015-02-16 23:05:00 +0200

    unzip downloaded data set

--- run_analysis.R
+++ run_analysis.R
@@ -1,7 +1,9 @@
+# Download and uncompress the data set if it doesn't exist already
 getUCIHARData <- function() {
     fname <- "UCI_HAR_Dataset.zip"
     if (!file.exists(fname)) {
         download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip",
-                      destfile="UCI_HAR_Dataset.zip", method="curl")
+                      destfile=fname, method="curl")
+        unzip(fname)
     }
 }
